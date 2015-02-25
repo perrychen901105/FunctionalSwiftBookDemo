fba0e260ad8a8321936cf2e47e8f05985653832e
perrychen901105 <chenzhipeng901105@gmail.com>
2015-02-25 21:19:09 +0800

    finish chapter two methods

--- Chapter2ThinkingFunctional.playground/section-1.swift
+++ Chapter2ThinkingFunctional.playground/section-1.swift
@@ -7,6 +7,8 @@ typealias Distance = CGFloat
 
 typealias Region = Position -> Bool
 
+let minimumDistance: Distance = 2.0
+
 func circle(radius: Distance) -> Region {
     return { point in sqrt(point.x * point.x + point.y * point.y) <= radius}
 }
@@ -28,3 +30,28 @@ func shift(offset: Position, region: Region) -> Region {
 
 
 shift(Position(x: 5, y: 5), circle(10))
+
+func invert(region: Region) -> Region {
+    return { point in !region(point) }
+}
+
+func intersection(region1: Region, region2: Region) -> Region {
+    return { point in region1(point) && region2(point) }
+}
+
+func union(region1: Region, region2: Region) -> Region {
+    return { point in region1(point) || region2(point) }
+}
+
+func difference(region: Region, minusRegion: Region) -> Region {
+    return intersection(region, minusRegion)
+}
+
+func inRange(ownPosition: Position, target: Position, friendly: Position, range: Distance) -> Bool {
+    let rangeRegion = difference(circle(range), circle(minimumDistance))
+    let targetRegion = shift(ownPosition, rangeRegion)
+    let friendlyRegion = shift(friendly, circle(minimumDistance))
+    let resultRegion = difference(targetRegion, friendlyRegion)
+    return resultRegion(target)
+}
+
