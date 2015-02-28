e8e92eea8b74887dbf8ff96557382e784776a67a
perrychen901105 <chenzhipeng901105@gmail.com>
2015-02-28 23:37:08 +0800

    add some code

--- Chapter5Optionals.playground/section-1.swift
+++ Chapter5Optionals.playground/section-1.swift
@@ -49,6 +49,77 @@ println(madridPopulation)
 //    }
 //}
 
+// Maps and more
+func incrementOptional(optional: Int?) -> Int? {
+    if let x = optional {
+        return x + 1
+    } else {
+        return nil
+    }
+}
 
+func map<T, U>(optional: T?, f: T -> U) -> U? {
+    if let x = optional {
+        return f(x)
+    } else {
+        return nil
+    }
+}
 
+func incrementOptional2(optional: Int?) -> Int? {
+    return optional.map {x in x + 1}
+}
+
+incrementOptional2(nil)
+
+//let x: Int? = 3
+//let y: Int? = nil
+//let z: Int? = x + y
+
+func addOptionals(optionalX: Int?, optionalY: Int?) -> Int? {
+    if let x = optionalX {
+        if let y = optionalY {
+            return x + y
+        }
+    }
+    return nil
+}
+var d = addOptionals(2, 3)
+
+let capitals = ["France": "Paris", "Spain": "Madrid", "The Netherlands": "Amsterdam", "Belgium": "Brussels"]
+
+func populationOfCapital(country: String) -> Int? {
+    if let capital = capitals[country] {
+        if let population = cities[capital] {
+            return population * 1000
+        }
+    }
+    return nil
+}
+
+infix operator >>= {}
+
+func >>=<U, T>(optional: T?, f: T -> U?) -> U? {
+    if let x = optional {
+        return f(x)
+    } else {
+        return nil
+    }
+}
+
+func addOptionals2(optionalX: Int?, optionalY: Int?) -> Int? {
+    return optionalX >>= { x in
+        optionalY >>= { y in
+            x + y
+        }
+    }
+}
+
+func populationOfCapital2(country: String) -> Int? {
+    return capitals[country] >>= { capital in
+        cities[capital] >>= { population in
+        return population * 1000
+        }
+    }
+}
 
