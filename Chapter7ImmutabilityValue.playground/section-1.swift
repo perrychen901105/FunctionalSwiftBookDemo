b58a27ec7d5a73c359a5b8915b88cff098bb0665
perrychen901105 <chenzhipeng901105@gmail.com>
2015-03-04 21:28:39 +0800

    add code

--- Chapter7ImmutabilityValue.playground/section-1.swift
+++ Chapter7ImmutabilityValue.playground/section-1.swift
@@ -11,5 +11,21 @@ var structPoint = PointStruct(x: 1, y: 2)
 var sameStructPoint = structPoint
 sameStructPoint.x = 3
 
+println("original is \(structPoint.x), and new is \(sameStructPoint.x)")
 
+class PointClass {
+    var x: Int
+    var y: Int
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+}
+
+var classPoint = PointClass(x: 1, y: 2)
+var sameClassPoint = classPoint
+sameClassPoint.x = 3
+
+println("original is \(classPoint.x), and new is \(sameClassPoint.x)")
 
