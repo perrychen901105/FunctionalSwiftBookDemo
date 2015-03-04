252914005ab4866ecc93f71630339b64d981c5ba
Perry <chenzhipeng901105@gmail.com>
2015-03-04 17:48:41 +0800

    add chapter 7 code

---
+++ Chapter7ImmutabilityValue.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+struct PointStruct {
+    var x: Int
+    var y: Int
+}
+
+var structPoint = PointStruct(x: 1, y: 2)
+var sameStructPoint = structPoint
+sameStructPoint.x = 3
+
+
+
