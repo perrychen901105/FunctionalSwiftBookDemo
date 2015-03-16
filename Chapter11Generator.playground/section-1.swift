2f647c44befbddbd236f5d69f29443dec567955b
Perry <chenzhipeng901105@gmail.com>
2015-03-16 16:10:26 +0800

    finish chapter 11 code

--- Chapter11Generator.playground/section-1.swift
+++ Chapter11Generator.playground/section-1.swift
@@ -50,4 +50,126 @@ func findPower(predicate: NSDecimalNumber -> Bool) -> NSDecimalNumber {
 
 println("\(findPower{$0.integerValue > 1000})")
 
+class FileLinesGenerator: GeneratorType {
+    typealias Element = String
+    
+    var lines: [String]
+    
+    init(filename: String) {
+        if let contents = String(contentsOfFile: filename, encoding: NSUTF8StringEncoding, error: nil) {
+            let newLine = NSCharacterSet.newlineCharacterSet()
+            lines = contents.componentsSeparatedByCharactersInSet(newLine)
+        } else {
+            lines = []
+        }
+    }
+    
+    func next() -> Element? {
+        if let nextLine = lines.first {
+            lines.removeAtIndex(0)
+            return nextLine
+        } else {
+            return nil
+        }
+    }
+}
+
+func find <G: GeneratorType>(var generator:G, predicate: G.Element -> Bool) -> G.Element? {
+    while let x = generator.next() {
+        if predicate(x) {
+            return x
+        }
+    }
+    return nil
+}
+
+class LimitGenerator<G: GeneratorType>: GeneratorType {
+    typealias Element = G.Element
+    var limit = 0
+    var generator: G
+    
+    init(limit: Int, generator: G) {
+        self.limit = limit
+        self.generator = generator
+    }
+    
+    func next() -> Element? {
+        if limit >= 0 {
+            limit--
+            return generator.next()
+        } else {
+            return nil
+        }
+    }
+}
+
+// rewrite the countdown generator
+func countDown(start: Int) -> GeneratorOf<Int> {
+    var i = start
+    return GeneratorOf {return i < 0 ? nil : i--}
+}
+
+func +<A>(var first: GeneratorOf<A>,
+    var second: GeneratorOf<A>) -> GeneratorOf<A> {
+        return GeneratorOf {
+            if let x = first.next() {
+                return x
+            } else if let x = second.next() {
+                return x
+            }
+            return nil
+        }
+}
+
+protocol SequenceType {
+    typealias Generator: GeneratorType
+    func generate() -> Generator
+}
+
+struct ReverseSequence<T>: SequenceType {
+    var array: [T]
+    init(array: [T]) {
+        self.array = array
+    }
+    
+    typealias Generator = CountdownGenerator
+    func generate() -> Generator {
+        return CountdownGenerator(array: array)
+    }
+}
+
+let reverseSequence = ReverseSequence(array: xs)
+let reverseGenerator = reverseSequence.generate()
+while let i = reverseGenerator.next() {
+    println("Index \(i) t is \(xs[i])")
+}
+
+
+func removeElement<T>(var array: [T]) -> GeneratorOf<[T]> {
+    var i = 0
+    return GeneratorOf {
+        if i < array.count {
+            var result = array
+            println("\(i)")
+            result.removeAtIndex(i)
+            i++
+            println("\(result)")
+            return result
+        }
+        return nil
+    }
+}
+
+Array(removeElement([1, 2 ,3]))
+
+/**
+*  Beyond Map and filter
+*/
+
+//struct SequenceOf<T> : SequenceType {
+//    init<G: GeneratorType>(_ makeUnderlyingGenerator: () -> G)
+//    func generate() -> GeneratorOf<T>
+//}
+
+
 
